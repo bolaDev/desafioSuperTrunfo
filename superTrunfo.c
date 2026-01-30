5a79750eaa1bb1c9b7fc3a1425547337b5f38a63
bolaDev <60231147+bolaDev@users.noreply.github.com>
2026-01-30 19:09:42 +0000

    fix bugs 2

--- superTrunfo.c
+++ superTrunfo.c
@@ -9,6 +9,14 @@ int main(){
     float pib;
     int pontoTuristico;
 
+    char estado2;
+    char  codigoCarta2[3];
+    char nomeCidade2[15];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontoTuristico2;
+
     printf("Desafio Super Trunfo! \n");
 
     printf("Este é o desafio Super Trunfo, insira os dados a seguir \n");
@@ -41,6 +49,35 @@ int main(){
     printf("Area: %.2f \n",area);
     printf("Pib: %.2f \n",pib);
     printf("Pontos turisticos: %d \n",pontoTuristico);
+
+    printf("Carta 2\n");
+    printf("Estado: ");
+    scanf(" %c",&estado2);
+    printf("Código da carta: ");
+    scanf("%s",codigoCarta2);   
+    printf("Nome da cidade: ");
+    scanf("%s",&nomeCidade2);
+    printf("População: ");
+    scanf("%d",&populacao2);
+    printf("Area: ");
+    scanf("%f",&area2);
+    printf("Pib: ");
+    scanf("%f",&pib2);
+    printf("Pontos turisticos: ");
+    scanf("%d",&pontoTuristico2);
+    printf("\n");
+    printf("------------------------\n");
+    printf("\n");
+
+    printf("Carta: 2 \n");
+    printf("Estado: %c \n",estado2);
+    printf("Codigo da carta: %s \n",codigoCarta2);
+    printf("Nome da cidade: %s \n",nomeCidade2);
+    printf("População: %d \n",populacao2);
+    printf("Area: %.2f \n",area2);
+    printf("Pib: %.2f \n",pib2);
+    printf("Pontos turisticos: %d \n",pontoTuristico2);
+
     
     return 0;
 }
